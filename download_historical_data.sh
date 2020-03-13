b80a1fed8d79abea364b61fae33e68303d4a795a
Mathieu Gouin <mgouin@gmail.com>
2020-03-13 18:10:28 -0400

    simplified download script

--- download_historical_data.sh
+++ download_historical_data.sh
@@ -1,24 +1,19 @@
 #!/bin/bash
 
-# Test
 echo Test
 python getYahooFinanceData.py -c 1 -f stock_db/test.txt -d ./stock_db/test
 
-# Indices
+echo Indices
 python getYahooFinanceData.py -c 1 -f stock_db/indices.txt -d ./stock_db/indices
 
-# Questrade portfolio
 echo Questrade
 python getYahooFinanceData.py -f stock_db/qt.txt -d ./stock_db/qt
 
-# Dow Jones
 echo Dow Jones
 python getYahooFinanceData.py -f stock_db/dj.txt -d ./stock_db/dj
 
-# TSX
 echo TSX
 python getYahooFinanceData.py -f stock_db/tsx.txt -d ./stock_db/tsx
 
-# SP-500
 echo SP-500
 python getYahooFinanceData.py -f stock_db/sp500.txt -d ./stock_db/sp500
