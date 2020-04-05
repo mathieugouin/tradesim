82b5bfa3a672cc80ca6d5a9e8f07d82f9cbeb803
Mathieu Gouin <mgouin@gmail.com>
2020-04-05 15:14:13 -0400

    codacy fix

--- tester.py
+++ tester.py
@@ -1,11 +1,13 @@
 import numpy as np
 import technical_indicators as ti
 
+
 def _main():
-    T = np.arange(-10,11,1)
+    T = np.arange(-10, 11, 1)
     print T
     print ti.step(T)
     print ti.ramp(T)
+
 
 if __name__ == '__main__':
     _main()
