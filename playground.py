368523138370b0770d7af436e135000e1a7fa497
Mathieu Gouin <mgouin@gmail.com>
2020-04-05 21:34:10 -0400

    Codacy fix

--- playground.py
+++ playground.py
@@ -2,14 +2,10 @@ from copy import deepcopy
 import pandas as pd
 import numpy as np
 import datetime
-import math
-import scipy as sp
 import scipy.stats as sps
 
 # user
-import finance_utils as fu
 import stock_db_mgr as sdm
-import tmxstockquote as tsq
 
 startdate = datetime.date(2014, 1, 6) # Start of Questrade portfolio
 #startdate = datetime.date(2013, 8, 12) # Start of Questrade portfolio component highest start date (VUN.TO)
@@ -35,53 +31,6 @@ def indicator_test():
     # Price in the floor (5% tolerance) & 15% drop
     print t.loc[(rp < 0.05) & (rr > 0.15)]
 
-    pass
-
-# TBD bad bad bad design... math does not work
-def qt_test():
-    db = sdm.CStockDBMgr('./stock_db/qt', startdate, enddate)
-    print "Loading all symbols..."
-    df = db.getAllSymbolDataSingleItem('Close')
-    print "Loading done."
-
-    # Base ratio
-    ratio = {
-        'XBB.TO': 0.1,
-        'ZCN.TO': 0.3,
-        'VUN.TO': 0.3,
-        'XEF.TO': 0.2,
-        'XEC.TO': 0.1
-    }
-
-    df.dropna(inplace=True)
-
-    pf = (df * ratio).sum(axis=1)
-
-    g1 = (pf[-1] - pf[0]) / pf[0]
-
-    newG = g1
-
-    keys = ratio.keys()
-    keys.sort()
-
-    for i in range(100):
-        #print "Iteration ", i
-        r = np.random.random(len(ratio))
-        r = (r / r.sum())  # Make sure sum = 1.0
-
-        ratio2 = dict(zip(keys, r))
-
-        pf2 = (df * ratio2).sum(axis=1)
-
-        g2 = (pf2[-1] - pf2[0]) / pf2[0]
-
-        if g2 > newG:
-            newRatio = deepcopy(ratio2)
-            newG = g2
-            print "New ratio:", newG, newRatio
-
-    #pd.concat([pf, pf2], axis=1).plot()
-    pass
 
 def correlation_test():
     db = sdm.CStockDBMgr('./stock_db/qt', startdate, enddate)
@@ -103,10 +52,11 @@ def correlation_test():
     print dfc.idxmin()
     pass
 
+
 def _main():
     indicator_test()
-    qt_test()
     correlation_test()
+
 
 if __name__ == '__main__':
     _main()
