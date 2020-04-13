165d1f5583ffbbbc626fd7ca416842b9c096eed4
Mathieu Gouin <mgouin@gmail.com>
2020-04-13 16:49:01 -0400

    lint: removed deprecated optparse module

--- get_yahoo_finance_data.py
+++ get_yahoo_finance_data.py
@@ -22,7 +22,7 @@ import sys
 import threading
 import Queue
 import datetime
-from optparse import OptionParser
+import argparse
 import os
 
 # User
@@ -50,9 +50,9 @@ class WorkerThread(threading.Thread):
             except Queue.Empty:
                 raise SystemExit
             if ticker[0] == "^": # make sure filename compatible
-                filenameTicker = ticker[1:]
+                filename_ticker = ticker[1:]
             else:
-                filenameTicker = ticker
+                filename_ticker = ticker
 
             if options.verbose:
                 print("ticker:", ticker)
@@ -61,12 +61,12 @@ class WorkerThread(threading.Thread):
 
             if not options.offline:
                 # download ticker data using yqd
-                alllines = yqd.load_yahoo_quote(ticker, fromdate, todate)
+                all_lines = yqd.load_yahoo_quote(ticker, fromdate, todate)
 
-                if len(alllines) > 5: # safety check
-                    filename = os.path.join(options.downloadTo, filenameTicker + '.csv')
+                if len(all_lines) > 5: # safety check
+                    filename = os.path.join(options.dir, filename_ticker + '.csv')
                     fp = open(filename, "wb")
-                    fp.write(alllines)
+                    fp.write(all_lines)
                     fp.close()
 
             if options.verbose:
@@ -80,67 +80,64 @@ if __name__ == '__main__':
     # today is
     today = datetime.datetime.now().strftime("%Y%m%d")
     # default start date (very early to get all possible data)
-    startdate = datetime.date(1900, 1, 1).strftime("%Y%m%d")
+    start_date = datetime.date(1900, 1, 1).strftime("%Y%m%d")
 
     # parse arguments
-    parser = OptionParser()
-    parser.add_option("-f", "--file", dest="tickerfile", action="store", default="./tickers.txt",
-                      help="read ticker list from file, it uses ./tickers.txt as default")
-    parser.add_option("-c", "--concurrent", type="int", dest="connections", default=10,
-                      action="store", help="# of concurrent connections")
-    parser.add_option("-d", "--dir", dest="downloadTo", action="store", default="./rawdata",
-                      help="save data to this directory, it uses ./rawdata/ as default")
+    parser = argparse.ArgumentParser(description='Yahoo historical quotes downloader')
+    parser.add_argument('-f', '--file', action='store', default='./tickers.txt',
+                        help='read ticker list from file, it uses ./tickers.txt as default')
+    parser.add_argument('-c', '--concurrent', type=int, default=10,
+                        action='store', help='# of concurrent connections used for the download')
+    parser.add_argument('-d', '--dir', action='store', default='./rawdata',
+                        help='save data to this directory, it uses ./rawdata/ as default')
+    parser.add_argument('-s', '--startdate', default=start_date, action='store',
+                        help='start date, format is YYYYMMDD, default is ' + start_date)
+    parser.add_argument('-t', '--todate', default=today, action='store',
+                        help='most recent date, format is YYYYMMDD, default is ' + today)
+    parser.add_argument('-v', '--verbose', action='store_true',
+                        help='explain what is being done')
+    parser.add_argument('-o', '--offline', action='store_true',
+                        help='used for testing, skip the actual downloading')
 
-    parser.add_option("-s", "--startdate", dest="startdate", default=startdate, action="store",
-                      help="start date, format is YYYYMMDD, ex: 19991231")
-    parser.add_option("-t", "--todate", dest="todate", default=today, action="store",
-                      help="most recent date needed, format is YYYYMMDD, ex: 20121231")
-    parser.add_option("-v", "--verbose", default=False,
-                          action="store_true", dest="verbose")
-    parser.add_option("-o", "--offline", default=False,
-                          action="store_true", dest="offline")
+    options = parser.parse_args()
 
-    (options, _args) = parser.parse_args()
+    if options.verbose:
+        print('Command line options parsed successfully.')
 
     # get input list
-    try:
-        f = open(options.tickerfile, 'r')
-        tickers = f.readlines()
-        f.close()
-    except Exception:
-        parser.error("ticker file %s not found" % options.tickerfile)
-        raise SystemExit
+    with open(options.file, 'r') as f:
+        ticker_lines = f.readlines()
 
     # build a queue with (ticker, fromdate, todate) tuples
     queue = Queue.Queue()
-    for tickerRow in tickers:
-        #print(tickerRow)
-        tickerRow = tickerRow.strip() # remove leading and trailing whitespace
-        if not tickerRow or tickerRow[0] == "#":  # skip comment line starting with #
+    for line in ticker_lines:
+        line = line.strip() # remove leading and trailing whitespace
+        # skip empty lines or line starting with # (comment)
+        if not line or line[0] == "#":
+            if options.verbose:
+                print('Skipping ticker file line: ' + line)
             continue
         # split on whitespace to ignore optional description after the ticker
-        tickerSplit = tickerRow.split()
+        ticker = line.split()[0]
 
         if options.verbose:
-            print("Adding {} from {} to {}".format(
-                        tickerSplit[0], options.startdate, options.todate))
+            print("Adding {} from {} to {}".format(ticker, options.startdate, options.todate))
 
-        # ticker, fromdate, todate
-        queue.put((tickerSplit[0], options.startdate, options.todate))
+        queue.put((ticker, options.startdate, options.todate))
 
     # Check args
     _my_assert(queue.queue, "no Tickers given")
-    numTickers = len(queue.queue)
-    connections = min(options.connections, numTickers)
+    nb_tickers = len(queue.queue)
+    connections = min(options.concurrent, nb_tickers)
     _my_assert(1 <= connections <= 255, "too much concurrent connections asked")
 
     if options.verbose:
         print("----- Getting {} tickers using {} simultaneous connections -----".format(
-            numTickers, connections))
+            nb_tickers, connections))
 
     # Get a dummy small quote from Y! to get the crumb & cookie before the threads start.
     _my_assert(len(yqd.load_yahoo_quote('^GSPC', '20180212', '20180212')) > 5,
-        "Error: initial download did not work")
+               "Error: initial download did not work")
 
     # start a bunch of threads, passing them the queue of jobs to do
     threads = []
