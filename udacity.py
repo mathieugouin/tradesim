d0f2dbda4202132c79c3739bf677508c8902f29e
Mathieu Gouin <mgouin@gmail.com>
2020-04-14 00:16:18 -0400

    python 2/3 fix

--- udacity.py
+++ udacity.py
@@ -1,5 +1,8 @@
 # For online course: https://classroom.udacity.com/courses/ud501
 
+# To make print working for Python2/3
+from __future__ import print_function
+
 import datetime
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -17,9 +20,9 @@ db = sdm.StockDBMgr('./stock_db/test', start_date, end_date)
 
 def test_pandas():
     df = db.get_symbol_data('AAPL')
-    print df.head()
-    print df.describe()
-    print df[10:21]
+    print(df.head())
+    print(df.describe())
+    print(df[10:21])
 
 
 def test_plot_data():
@@ -33,32 +36,29 @@ def test_plot_data():
 
 def test_multi_symbol():
     df = db.get_all_symbol_single_data_item('Close')
-    print df[['AAPL', 'IBM']].max()
+    print(df[['AAPL', 'IBM']].max())
 
     df = db.get_all_symbol_single_data_item('Volume')
-    print df[['AAPL', 'IBM']].mean()
-    pass
+    print(df[['AAPL', 'IBM']].mean())
 
 
 def test_slice():
     df = db.get_all_symbol_single_data_item('Close')
 
     df_row = df.loc['2010-01-01':'2010-01-10']
-    print df_row
+    print(df_row)
 
     df_col = df[['GOOG', 'IBM']]
-    print df_col.head(1)  # too big
+    print(df_col.head(1))  # too big
 
     df_both = df.loc['2010-01-01':'2010-01-10', ['AAPL', 'SPY']]
-    print df_both
-    pass
+    print(df_both)
 
 
 def test_normalize():
-    df = db.get_all_symbol_single_data_item('Close')
+    df = db.get_all_symbol_single_data_item('Close').loc[:, ['AAPL', 'GLD', 'IBM', 'SPY']]
     df2 = fu.normalize_data_frame(df)
     plot_data(df2)
-    pass
 
 
 def get_data(symbols, dates):
@@ -94,33 +94,33 @@ def test_plot():
 def test_np_arrays():
     a1 = np.ones(4)
     a2 = np.zeros((3, 5))
-    print a1
-    print a2
+    print(a1)
+    print(a2)
 
     r1 = np.random.rand(2, 3)
     r2 = np.random.randn(2, 3)
-    print r1
-    print r2
+    print(r1)
+    print(r2)
 
     a = np.random.randn(5)
-    print a
+    print(a)
     indices = np.array([1,1,2,3])
-    print indices
-    print a[indices]
+    print(indices)
+    print(a[indices])
 
     a = np.array([(20,25,10,23,26,32,10,5,0), (0,2,50,20,0,1,28,5,0)])
-    print a
+    print(a)
     m = a.mean()
-    print m
-    print a[a < m]
+    print(m)
+    print(a[a < m])
     a[a < m] = -1
-    print a
+    print(a)
 
     a = np.array([(1,2,3,4,5), (10,20,30,40,50)])
-    print a
-    print 2 * a
-    print a / 2
-    print a / 2.0
+    print(a)
+    print(2 * a)
+    print(a / 2)
+    print(a / 2.0)
 
     pass
 
@@ -131,9 +131,9 @@ def test_time_series():
     df = get_data(symbols, dates)
     plot_data(df)
 
-    print 'Mean:', df.mean()
-    print 'Median', df.median()
-    print 'Std dev', df.std()
+    print('Mean: {}'.format(df.mean()))
+    print('Median: {}'.format(df.median()))
+    print('Std dev: {}'.format(df.std()))
 
 
 def test_rolling_stats():
@@ -186,8 +186,8 @@ def test_daily_returns():
 
     # Compute daily returns
     dr = df.pct_change() * 100.0  # make it directly in %
-    #dr.iloc[0] = 0.0 # asked by quiz, not sure it makes sense
-    plot_data(dr, title='daily returns', ylabel='%')
+    #dr.iloc[0] = 0.0  # asked by quiz, not sure it makes sense
+    plot_data(dr, title='Daily Returns', ylabel='%')
     #dr.plot(marker='.', linestyle='')  # alternate plot style
 
 
