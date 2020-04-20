310b3e54248405e18d24e199df0d6bce5dd878df
Mathieu Gouin <mgouin@gmail.com>
2020-04-20 00:03:37 -0400

    lint fix

--- test/test_stock_db_mgr.py
+++ test/test_stock_db_mgr.py
@@ -24,4 +24,3 @@ def test_get_all_symbols():
     db = sdm.StockDBMgr('./stock_db/test', datetime.date(2017, 1, 1), datetime.date(2018, 1, 1))
     symbol_list = db.get_all_symbols()
     assert len(symbol_list) > 0
-
