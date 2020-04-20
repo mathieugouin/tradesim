6d09bf91ea855b2b654568ce4470fd597fe3bae3
Mathieu Gouin <mgouin@gmail.com>
2020-04-20 15:49:55 -0400

    more sdm tests

--- test/test_stock_db_mgr.py
+++ test/test_stock_db_mgr.py
@@ -2,15 +2,6 @@ import datetime
 import stock_db_mgr as sdm
 
 
-def test_creation_custom_date_range():
-    start = datetime.date(2020, 3, 16)
-    stop = datetime.date(2020, 3, 20)
-    db = sdm.StockDBMgr('./stock_db/test', start, stop)
-    df = db.get_symbol_data('SPY')
-    assert df.iloc[0].name.date() == start
-    assert df.iloc[-1].name.date() == stop
-
-
 def test_creation_default_date_range():
     db = sdm.StockDBMgr('./stock_db/test')
     df = db.get_symbol_data('SPY')
@@ -18,6 +9,15 @@ def test_creation_default_date_range():
     stop = datetime.date(2020, 3, 20)
     assert df.iloc[0].name.date() < start
     assert df.iloc[-1].name.date() > stop
+
+
+def test_creation_custom_date_range():
+    start = datetime.date(2020, 3, 16)
+    stop = datetime.date(2020, 3, 20)
+    db = sdm.StockDBMgr('./stock_db/test', start, stop)
+    df = db.get_symbol_data('SPY')
+    assert df.iloc[0].name.date() == start
+    assert df.iloc[-1].name.date() == stop
 
 
 def test_get_all_symbols():
@@ -28,6 +28,7 @@ def test_get_all_symbols():
 
 def test_download_data():
     db = sdm.StockDBMgr('./stock_db/test')
+    assert 'SPY' not in db._dic
     db.get_symbol_data('SPY')
     assert 'SPY' in db._dic
     db.download_data('SPY')
@@ -40,8 +41,35 @@ def test_validate():
         assert db.validate_symbol_data(s)
 
 
+def test_update_all_symbols():
+    db = sdm.StockDBMgr('./stock_db/empty')
+    assert 'SPY' not in db._dic
+    # Get a single symbol
+    db.get_symbol_data('SPY')
+    assert 'SPY' in db._dic
+    db.update_all_symbols()
+    assert 'SPY' not in db._dic
+
+
+def test_get_symbol_data():
+    db = sdm.StockDBMgr('./stock_db/test')
+    df1 = db.get_symbol_data('SPY')
+    df2 = db.get_symbol_data('SPY')
+    assert df1 == df2
+
+
+def test_get_all_symbol_data():
+    db = sdm.StockDBMgr('./stock_db/test')
+    dic = db.get_all_symbol_data()
+    for s in db.get_all_symbols():
+        assert s in dic
+
+
 def test_get_all_symbol_single_data_item():
     db = sdm.StockDBMgr('./stock_db/test')
     df = db.get_all_symbol_single_data_item('Close')
     for s in db.get_all_symbols():
         assert s in df
+
+    df = db.get_all_symbol_single_data_item('BadData')
+    assert df is None
