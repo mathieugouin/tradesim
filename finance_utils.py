3b2004ffd46733a000cfaa9dcbd4b741b2bc099e
Mathieu Gouin <mgouin@gmail.com>
2020-04-24 17:21:25 -0400

    fix long line

--- finance_utils.py
+++ finance_utils.py
@@ -59,7 +59,8 @@ def download_url(url):
 
     # Headers to fake a user agent
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.101 Safari/537.36'
+        'User-Agent':   'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 ' \
+                        '(KHTML, like Gecko) Chrome/41.0.2272.101 Safari/537.36'
     }
 
     s = ""
