c5536d517a09d1f07b49a84daf980cc3696c555c
Mathieu Gouin <mgouin@gmail.com>
2020-04-25 15:00:22 -0400

    User Agent to yqd

--- yqd.py
+++ yqd.py
@@ -31,7 +31,8 @@ _crumb = None
 
 # Headers to fake a user agent
 _headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.101 Safari/537.36'
+    'User-Agent':   'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) ' \
+                    'Chrome/41.0.2272.101 Safari/537.36'
 }
 
 
