ae8e4331caed9c6e3035cca7b7d8224b82a5e58a
Mathieu Gouin <mgouin@gmail.com>
2023-06-19 00:40:15 +0000

    more robust df tests

--- test/test_finance_utils.py
+++ test/test_finance_utils.py
@@ -116,7 +116,7 @@ def test_update_all_symbols():
     assert len(fu.get_all_symbols(d)) == 1
 
 
-def test_load_dataframe():
+def test_load_dataframe_adj():
     f = 'stock_db/test/SPY.csv'
     df = fu.load_dataframe(f, datetime.date(2018, 1, 1), datetime.date(2018, 4, 1), True)
 
@@ -138,6 +138,24 @@ def test_load_dataframe_no_adj():
 
     for c in test_col:
         assert c in col
+
+
+def test_load_dataframe_date_check_1():
+    f = 'stock_db/test/SPY.csv'
+    start_date = datetime.date(2018, 1, 3)
+    stop_date = datetime.date(2018, 1, 17)
+    df = fu.load_dataframe(f, start_date, stop_date)
+    assert df.iloc[0].name.date() == start_date
+    assert df.iloc[-1].name.date() == stop_date
+
+
+def test_load_dataframe_date_check_2():
+    f = 'stock_db/test/SPY.csv'
+    start_date = datetime.date(1900, 1, 3)
+    stop_date = datetime.date(2100, 1, 17)
+    df = fu.load_dataframe(f, start_date, stop_date)
+    assert df.iloc[0].name.date() > start_date
+    assert df.iloc[-1].name.date() < stop_date
 
 
 def create_random_df():
