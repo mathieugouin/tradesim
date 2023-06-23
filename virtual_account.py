e058b98a2506731e909a1a04e2327691f5b98ab9
Mathieu Gouin <mgouin@gmail.com>
2023-06-23 00:03:36 +0000

    added pylintrc + some lint fixes

--- virtual_account.py
+++ virtual_account.py
@@ -1,8 +1,13 @@
+"""VirtualAccount
+This class provides the means to operate on a virtual account.
+Buy and sell operation can be performed.
+"""
+
 # To make print working for Python2/3
 from __future__ import print_function
 
 import math
-import position
+import position as pos
 import finance_utils as fu
 
 
@@ -18,6 +23,7 @@ class VirtualAccount(object):
         self._positions = []
 
     def buy_at_market(self, bar, symbol, nb_share, name="buy_at_market"):
+        """TBD bar vs date"""
         print("buy_at_market()")
         nb_share = math.floor(nb_share)
         if nb_share > 0:
@@ -25,7 +31,9 @@ class VirtualAccount(object):
             commission = fu.calc_commission(nb_share)
             cost = buy_price * nb_share + commission
             if cost < self._cash:
-                self._positions.append(position.Position(bar, symbol, nb_share, buy_price, name, commission))
+                self._positions.append(
+                    pos.Position(bar, symbol, nb_share,
+                        buy_price, name, commission))
                 self._cash -= cost
             else:
                 print("Error: not enough money")
@@ -33,6 +41,7 @@ class VirtualAccount(object):
             print("Error: can't buy 0 share")
 
     def sell_at_market(self, position, bar, name="sell_at_market"):
+        """TBD bar vs date"""
         print("sell_at_market()")
         if position.is_open():
             sell_price = self._data_dic[position.get_symbol()].iloc[bar]['Low']  # Worst case
@@ -46,6 +55,8 @@ class VirtualAccount(object):
             print("Error: position already closed")
 
     def get_all_positions(self, symbol=""):
+        """Return all positions for a given symbol,
+        or for all symbols if none are provided."""
         if symbol in self._data_dic:
             # positions only for symbol
             return [p for p in self._positions if p.get_symbol() == symbol]
@@ -53,6 +64,8 @@ class VirtualAccount(object):
         return self._positions
 
     def get_open_positions(self, symbol=""):
+        """Return all open positions (not sold) for a given symbol,
+        or for all symbols if none are provided."""
         if symbol in self._data_dic:
             # open positions only for symbol
             return [p for p in self._positions if p.get_symbol() == symbol and p.is_open()]
@@ -60,6 +73,8 @@ class VirtualAccount(object):
         return [p for p in self._positions if p.is_open()]
 
     def get_close_positions(self, symbol=""):
+        """Return all close positions (sold) for a given symbol,
+        or for all symbols if none are provided."""
         if symbol in self._data_dic:
             # close positions only for symbol
             return [p for p in self._positions if p.get_symbol() == symbol and not p.is_open()]
@@ -67,9 +82,11 @@ class VirtualAccount(object):
         return [p for p in self._positions if not p.is_open()]
 
     def get_cash(self):
+        """Return the current cash amount in the account."""
         return self._cash
 
     def delta_cash(self, delta):
+        """Perform a cash change operation without actually performing a trade."""
         self._cash += delta
         if self._cash < 0:
             print("Error: not enough money: {}".format(self._cash))
