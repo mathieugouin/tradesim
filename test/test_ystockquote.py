7a86c3f4de0275f9547de1677f2f4406c6e54a12
Mathieu Gouin <mgouin@gmail.com>
2023-07-18 16:33:06 +0000

    Latest version of y! finance should work

--- test/test_ystockquote.py
+++ test/test_ystockquote.py
@@ -2,7 +2,7 @@ import math
 import pytest
 import ystockquote as ysq
 
-pytest.skip("Skipping unstable ystockquote tests", allow_module_level=True)
+# pytest.skip("Skipping unstable ystockquote tests", allow_module_level=True)
 
 
 @pytest.mark.webtest
