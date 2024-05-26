3ca3d0098c7d1d491769f6c11d6263cff11fe2f0
Mathieu Gouin <mgouin@gmail.com>
2024-05-25 21:28:20 -0400

    Update test_ystockquote.py

--- tests/test_ystockquote.py
+++ tests/test_ystockquote.py
@@ -3,7 +3,7 @@ import pytest
 import ystockquote as ysq
 
 # To investigate...
-# pytest.skip("Skipping unstable ystockquote tests", allow_module_level=True)
+pytest.skip("Skipping unstable ystockquote tests", allow_module_level=True)
 
 
 @pytest.mark.webtest
