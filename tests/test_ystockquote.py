0f838f2d805d1583248ac250caf8a734a790416a
Mathieu Gouin <mgouin@gmail.com>
2024-06-12 01:33:31 +0000

    re-enable y!

--- tests/test_ystockquote.py
+++ tests/test_ystockquote.py
@@ -2,8 +2,6 @@ import math
 import pytest
 import ystockquote as ysq
 
-pytest.skip("Skipping unstable ystockquote tests", allow_module_level=True)
-
 
 @pytest.mark.webtest
 @pytest.mark.parametrize("i", ['', 'badInfo', 'volume ', ' volume'])
