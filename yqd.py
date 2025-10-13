ba86ce4260004b30b716b7880f4796f6eee80fbb
Mathieu Gouin <mgouin@gmail.com>
2025-10-13 02:10:52 +0000

    Handle exception+ stack trace

--- yqd.py
+++ yqd.py
@@ -1,24 +1,30 @@
 """yqd.py - Yahoo Quote Downloader."""
 
 import yfinance as yf
+import traceback
 
 
 def load_yahoo_quote(ticker, begindate, enddate, filename):
-    """Loads the corresponding history from Yahoo.
+    """Load the corresponding history from Yahoo.
 
-    The "begindate" and "enddate" are strings in the format of YYYYMMDD are are inclusive.
+    The "begindate" and "enddate" are strings in the format of YYYYMMDD
+    and are are inclusive.
 
     The downloaded data is written to filename.
     """
 
     def to_iso_date(yyyymmdd):
-        return yyyymmdd[0:4] + '-' + yyyymmdd[4:6] + '-' + yyyymmdd[6:]
+        return yyyymmdd[0:4] + "-" + yyyymmdd[4:6] + "-" + yyyymmdd[6:]
 
-    df = yf.download(
-        tickers=ticker,
-        start=to_iso_date(begindate), end=to_iso_date(enddate),
-        group_by='ticker', multi_level_index=False,
-        progress=False, auto_adjust=True, back_adjust=True)
+    try:
+        df = yf.download(
+            tickers=ticker,
+            start=to_iso_date(begindate), end=to_iso_date(enddate),
+            group_by="ticker", multi_level_index=False,
+            progress=False, auto_adjust=True, back_adjust=True)
 
-    if len(df) > 0:
-        df.to_csv(filename)
+        if len(df) > 0:
+            df.to_csv(filename)
+    except Exception as e:
+        print(f"Error: failed to download {ticker}. {e}")
+        traceback.print_exc()
